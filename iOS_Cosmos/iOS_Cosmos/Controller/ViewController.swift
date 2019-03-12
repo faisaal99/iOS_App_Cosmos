e32bd30459f9d083dbd2057ee4f577046165c0b7
Faisal A <faisal.163@hotmail.com>
2019-03-12 22:18:22 +0100

    Adding the base-project
    Only contains the viewcontroller with a small modification.

---
+++ iOS_Cosmos/iOS_Cosmos/Controller/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iOS_Cosmos
+//
+//  Created by Faisal Akhtar on 2019-03-12.
+//  Copyright © 2019 sti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var lblTitle: UILabel! // COSMOS-logo
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+    
+    
+
+
+}
+
