96e67e917b9b31862f217b22b6c8d4510392fe88
Faisal A <faisal.163@hotmail.com>
2019-04-13 17:30:59 +0200

    Small update to title screen.
    Added some extra animations to the title screen. COSMOS-label will get replaced by an actual logo.

---
+++ iOS_Cosmos/iOS_Cosmos/Controller/MainMenuPageController.swift
@@ -0,0 +1,29 @@
+//
+//  MainMenuPageController.swift
+//  iOS_Cosmos
+//
+//  Created by Faisal Akhtar on 2019-04-13.
+//  Copyright © 2019 sti. All rights reserved.
+//
+
+import UIKit
+
+class MainMenuPageController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- iOS_Cosmos/iOS_Cosmos/Controller/ViewController.swift
+++ iOS_Cosmos/iOS_Cosmos/Controller/ViewController.swift
@@ -11,17 +11,28 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var lblTitle: UILabel! // COSMOS-logo
+    @IBOutlet weak var btnContinue: UIButton!
     
+    var titleCenterX: CGFloat?
+    var titleCenterY: CGFloat?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
+        titleCenterX = CGFloat(self.view.frame.size.width / 2 - self.lblTitle.frame.size.width / 2)
+        titleCenterY = CGFloat(self.view.frame.size.height / 2 - self.lblTitle.frame.size.height / 2)
         
+        lblTitle.frame = CGRect(x: titleCenterX!, y: titleCenterY!, width: 174, height: 48)
+        btnContinue.alpha = 1
+    }
+    
+    @IBAction func continueButtonPressed(_ sender: Any) {
+        UIView.animate(withDuration: 1.2) {
+            self.lblTitle.frame = CGRect(x: self.titleCenterX!, y: 90, width: 174, height: 48)
+            self.btnContinue.alpha = 0
+        }
     }
     
     
-
-
 }
 
