7697c660b05841d0fbe46f9507628de53a5fb190
Faisal A <faisal.163@hotmail.com>
2019-04-16 21:13:52 +0200

    New Info Window and buttons
    Added an info window, but it doesn't yet show the planet data. Buttons for every page are now there with constraints.

---
+++ iOS_Cosmos/iOS_Cosmos/Controller/PlanetInfoViewController.swift
@@ -0,0 +1,33 @@
+//
+//  PlanetInfoViewController.swift
+//  iOS_Cosmos
+//
+//  Created by Faisal Akhtar on 2019-04-16.
+//  Copyright © 2019 sti. All rights reserved.
+//
+
+import UIKit
+
+class PlanetInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+
+    @IBAction func exitInfoPressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
